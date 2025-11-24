b96bbbdea9819a2c4513fd3f0dce75e8ed0f4819
julio <juliocesar13b@gmail.com>
2025-11-23 21:58:56 -0300

    ajustando dto de registro de usuario

--- src/main/java/com/financas/julio/dto/UserRegisterRequest.java
+++ src/main/java/com/financas/julio/dto/UserRegisterRequest.java
@@ -3,7 +3,9 @@ package com.financas.julio.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+import java.time.LocalDateTime;
+
 public record UserRegisterRequest(@NotBlank(message = "Name is mandatory") String name,
-                          @Email @NotBlank(message = "Email is mandatory") String email,
-                          @NotBlank(message = "Password is mandatory") String password) {
+                                  @Email @NotBlank(message = "Email is mandatory") String email,
+                                  @NotBlank(message = "Password is mandatory") String password) {
 }
--- src/main/java/com/financas/julio/dto/UserRegisterResponse.java
+++ src/main/java/com/financas/julio/dto/UserRegisterResponse.java
@@ -1,5 +1,8 @@
 package com.financas.julio.dto;
 
+import java.time.LocalDateTime;
+
 public record UserRegisterResponse(String name,
-                                   String email) {
+                                   String email,
+                                   LocalDateTime dataCadastro) {
 }
