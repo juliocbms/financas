5e18a48fcc89d7d09cc1654e9d546bc7fcca3988
julio <juliocesar13b@gmail.com>
2025-11-23 22:01:05 -0300

    ajustando mapper e repository

--- src/main/java/com/financas/julio/mappers/UserMapper.java
+++ src/main/java/com/financas/julio/mappers/UserMapper.java
@@ -3,13 +3,22 @@ package com.financas.julio.mappers;
 import com.financas.julio.dto.UserRegisterRequest;
 import com.financas.julio.dto.UserRegisterResponse;
 
+import com.financas.julio.dto.UserUpdateRequest;
+import com.financas.julio.dto.UserUpdateResponse;
 import com.financas.julio.model.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     @Mapping(target = "id", ignore = true)
     User toEntity(UserRegisterRequest request);
+
+    @Mapping(source = "createdAt", target = "dataCadastro")
     UserRegisterResponse toResponse(User user);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    User updateToEntity(UserUpdateRequest request, @MappingTarget User entity);
+
+    UserUpdateResponse updateTOResponse(User user);
 }
