cb409c1d4b141e7ddf3a5ff3cd476ce9f9ca0549
julio <juliocesar13b@gmail.com>
2025-11-29 19:51:54 -0300

    criando consultas personalizadas

--- src/main/java/com/financas/julio/repository/ContaRepository.java
+++ src/main/java/com/financas/julio/repository/ContaRepository.java
@@ -3,9 +3,13 @@ package com.financas.julio.repository;
 import com.financas.julio.model.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContaRepository extends JpaRepository<Conta,Long> {
 
     boolean existsByUserIdAndNameIgnoreCase(Long userId, String name);
+
+    List<Conta> findByUserId(Long userId);
 
     long countByUserId(Long userId);
 }
