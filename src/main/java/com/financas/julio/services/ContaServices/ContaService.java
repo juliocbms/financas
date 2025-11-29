d8444b5abda4fbe0039d3bb508c816c80eed1432
julio <juliocesar13b@gmail.com>
2025-11-29 19:20:54 -0300

    criando metodos e validacoes

--- src/main/java/com/financas/julio/services/ContaServices/ContaService.java
+++ src/main/java/com/financas/julio/services/ContaServices/ContaService.java
@@ -1,15 +1,19 @@
 package com.financas.julio.services.ContaServices;
 
 import com.financas.julio.dto.contaDTO.ContaRegisterRequest;
+import com.financas.julio.dto.contaDTO.ContaUpdateRequest;
 import com.financas.julio.mappers.ContaMapper;
 import com.financas.julio.model.Conta;
 import com.financas.julio.model.User;
 import com.financas.julio.repository.ContaRepository;
 import com.financas.julio.repository.UserRepository;
-
+import com.financas.julio.services.exception.RegraNegocioException;
+import com.financas.julio.services.exception.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,12 +30,84 @@ public class ContaService {
         this.userRepository = userRepository;
     }
 
+    private static final int LIMITE_MAXIMO_CONTAS = 10;
 
+
+    @Transactional
     public Conta insertConta(ContaRegisterRequest request){
-        Conta conta = mapper.toEntity(request);
+        validarUsuarioExiste(request.usuarioId());
+        validarLimiteDeContas(request.usuarioId());
+        validarNomeDuplicado(request.usuarioId(), request.name());
 
         User usuario = userRepository.getReferenceById(request.usuarioId());
+        Conta conta = mapper.toEntity(request);
         conta.setUser(usuario);
-        return contaRepository.save(conta);
+        logger.info("Trying to register a user account");
+            return contaRepository.save(conta);
+    }
+
+    @Transactional
+    public void deleteConta(Long id){
+        Conta existingAccount = findAccountOrThrow(id);
+        if (existingAccount.getId() == null){
+            logger.info("Resource with id "+id+ " was deleted");
+            contaRepository.deleteById(id);
+        }
+        try {
+            logger.info("Resource with id "+id+ " was deleted");
+            contaRepository.deleteById(id);
+        } catch (IllegalArgumentException e){
+            logger.warn("Invalid arguments");
+            throw e;
+        } catch (DataIntegrityViolationException ex){
+            throw ex;
+        }
+    }
+
+    public Conta updateAccount(Long id, ContaUpdateRequest request){
+        Conta existingAccount = findAccountOrThrow(id);
+        try {
+            logger.info("Updating account with id: " + id);
+            mapper.updateToEntity(request, existingAccount);
+            return contaRepository.save(existingAccount);
+        } catch (IllegalArgumentException e){
+            logger.warn("Invalid arguments");
+            throw e;
+        } catch (DataIntegrityViolationException ex) {
+            logger.error("Data integrity violation", ex);
+            throw ex;
+        }
+    }
+
+
+    private void validarUsuarioExiste(Long usuarioId) {
+        if (!userRepository.existsById(usuarioId)) {
+            throw new EntityNotFoundException("Usuário não encontrado com o ID: " + usuarioId);
+        }
+    }
+
+    private void validarLimiteDeContas(Long usuarioId) {
+        long totalAtual = contaRepository.countByUserId(usuarioId);
+
+        if (totalAtual >= LIMITE_MAXIMO_CONTAS) {
+            throw new RegraNegocioException(
+                    String.format("Limite atingido. O usuário já possui %d contas (Máximo: %d)", totalAtual, LIMITE_MAXIMO_CONTAS)
+            );
+        }
+    }
+
+    private void validarNomeDuplicado(Long usuarioId, String nomeConta) {
+        if (contaRepository.existsByUserIdAndNameIgnoreCase(usuarioId, nomeConta)) {
+            throw new RegraNegocioException("Já existe uma conta cadastrada com o nome '" + nomeConta + "'");
+        }
+    }
+
+    private Conta findAccountOrThrow(Long id){
+        logger.info("Searching for account with id: {}", id);
+        return contaRepository.findById(id)
+                .orElseThrow(() -> {
+                    logger.warn("Account not found with id: {}", id);
+                    return new ResourceNotFoundException(id);
+                });
     }
 }
