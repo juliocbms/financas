2c59989f972f18b4fbadb94b000139e71c3a7205
julio <juliocesar13b@gmail.com>
2025-11-29 17:19:25 -0300

    criando mapper de conta

--- src/main/java/com/financas/julio/mappers/ContaMapper.java
+++ src/main/java/com/financas/julio/mappers/ContaMapper.java
@@ -1,4 +1,21 @@
 package com.financas.julio.mappers;
 
+import com.financas.julio.dto.contaDTO.ContaRegisterRequest;
+import com.financas.julio.dto.contaDTO.ContaResponse;
+import com.financas.julio.model.Conta;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface ContaMapper {
+
+    @Mapping(target = "usuario", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "criadoEm", ignore = true)
+    @Mapping(target = "atualizadoEm", ignore = true)
+    @Mapping(target = "saldoAtual", source = "saldoInicial")
+    Conta toEntity(ContaRegisterRequest request);
+
+    @Mapping(source = "usuario.id", target = "usuarioId")
+    ContaResponse toResponse(Conta conta);
 }
