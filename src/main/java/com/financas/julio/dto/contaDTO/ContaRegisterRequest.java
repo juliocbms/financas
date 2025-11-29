da7556f8886477e8e7e1b8f4062caa0a62e43be5
julio <juliocesar13b@gmail.com>
2025-11-29 17:18:33 -0300

    criando dto para criação de contas

--- src/main/java/com/financas/julio/dto/contaDTO/ContaRegisterRequest.java
+++ src/main/java/com/financas/julio/dto/contaDTO/ContaRegisterRequest.java
@@ -1,4 +1,24 @@
 package com.financas.julio.dto.contaDTO;
 
-public class InsertContaDTO {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+
+public record ContaRegisterRequest(@NotNull(message = "O ID do usuário é obrigatório")
+                                   Long usuarioId,
+
+                                   @NotBlank(message = "O nome da conta não pode estar vazio")
+                                   @Size(min = 3, max = 120, message = "O nome deve ter entre 3 e 120 caracteres")
+                                   String nome,
+
+                                   @NotBlank(message = "O tipo da conta não pode estar vazio")
+                                   @Size(max = 100, message = "O tipo da conta deve ter no máximo 100 caracteres")
+                                   String tipoConta,
+
+                                   @PositiveOrZero(message = "O saldo inicial não pode ser negativo")
+                                   BigDecimal saldoInicial
+) {
 }
--- src/main/java/com/financas/julio/dto/contaDTO/ContaResponse.java
+++ src/main/java/com/financas/julio/dto/contaDTO/ContaResponse.java
@@ -1,4 +1,15 @@
 package com.financas.julio.dto.contaDTO;
 
-public record ContaResponse() {
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record ContaResponse(Long id,
+                            Long usuarioId,
+                            String nome,
+                            String tipoConta,
+                            BigDecimal saldoAtual,
+                            @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+                            LocalDateTime criadoEm) {
 }
