43cd6d0bc26f01e1f81a6a1753eaecc8df76a10f
julio <juliocesar13b@gmail.com>
2025-12-07 18:29:06 -0300

    adicionando testes de categoria

---
+++ src/test/java/com/financas/julio/services/CategoriaServices/CategoriaServiceTest.java
@@ -0,0 +1,185 @@
+package com.financas.julio.services.CategoriaServices;
+
+import com.financas.julio.dto.categoriaDTO.CategoriaRegisterRequest;
+import com.financas.julio.dto.categoriaDTO.CategoriaResponse;
+import com.financas.julio.dto.categoriaDTO.CategoriaUpdateRequest;
+import com.financas.julio.mappers.CategoriaMapper;
+import com.financas.julio.model.Categoria;
+import com.financas.julio.model.TipoCategoria;
+import com.financas.julio.model.User;
+import com.financas.julio.repository.CategoriaRepository;
+import com.financas.julio.repository.ContaRepository;
+import com.financas.julio.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class CategoriaServiceTest {
+
+    @InjectMocks
+    private CategoriaService service;
+
+    @Mock
+    private CategoriaRepository categoriaRepository;
+
+    @Mock
+    private CategoriaMapper mapper;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private ContaRepository contaRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void insertCategoria() {
+        Long usuarioId = 1L;
+        String nomeCategoria = "Lazer";
+
+        CategoriaRegisterRequest request = new CategoriaRegisterRequest(usuarioId, nomeCategoria, TipoCategoria.RECEITA);
+        User user = new User();
+        user.setId(usuarioId);
+
+        Categoria categoria = new Categoria();
+        categoria.setName(nomeCategoria);
+        categoria.setUser(user);
+
+        Mockito.when(categoriaRepository.existsByUserIdAndNameIgnoreCase(usuarioId, nomeCategoria)).thenReturn(false);
+
+        Mockito.when(mapper.toEntity(request)).thenReturn(categoria);
+
+        Mockito.when(userRepository.getReferenceById(usuarioId)).thenReturn(user);
+
+        Mockito.when(categoriaRepository.save(categoria)).thenReturn(categoria);
+
+        Categoria result = service.insertCategoria(request);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(nomeCategoria, result.getName());
+        Assertions.assertEquals(usuarioId, result.getUser().getId());
+
+        Mockito.verify(categoriaRepository).save(categoria);
+    }
+
+    @Test
+    void deleteCategoria() {
+        Long categoriaId = 10L;
+        Long usuarioId = 1L;
+
+        User user = new User();
+        user.setId(usuarioId);
+
+        Categoria categoria = new Categoria();
+        categoria.setId(categoriaId);
+        categoria.setUser(user);
+
+        Mockito.when(categoriaRepository.findById(categoriaId)).thenReturn(Optional.of(categoria));
+
+        Mockito.doNothing().when(categoriaRepository).delete(categoria);
+
+        service.deleteCategoria(categoriaId, usuarioId);
+
+        Mockito.verify(categoriaRepository).delete(categoria);
+    }
+
+    @Test
+    void updateSelfCategoria() {
+        Long categoriaId = 10L;
+        Long usuarioId = 1L;
+        String novoNome = "Lazer Atualizado";
+
+        User user = new User();
+        user.setId(usuarioId);
+
+        Categoria categoriaAntiga = new Categoria();
+        categoriaAntiga.setId(categoriaId);
+        categoriaAntiga.setName("Lazer Antigo");
+        categoriaAntiga.setUser(user);
+
+        CategoriaUpdateRequest request = new CategoriaUpdateRequest(novoNome,TipoCategoria.RECEITA);
+
+        Mockito.when(categoriaRepository.findById(categoriaId)).thenReturn(Optional.of(categoriaAntiga));
+
+        Mockito.when(categoriaRepository.existsByUserIdAndNameIgnoreCase(usuarioId, novoNome)).thenReturn(false);
+
+        Mockito.when(categoriaRepository.save(categoriaAntiga)).thenReturn(categoriaAntiga);
+
+        Categoria result = service.updateSelfCategoria(categoriaId, request, usuarioId);
+
+        Mockito.verify(mapper).updateToEntity(request, categoriaAntiga);
+        Mockito.verify(categoriaRepository).save(categoriaAntiga);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void findById() {
+        Long categoriaId = 10L;
+        Long usuarioId = 1L;
+
+        User user = new User();
+        user.setId(usuarioId);
+
+        Categoria categoria = new Categoria();
+        categoria.setId(categoriaId);
+        categoria.setUser(user);
+
+        Mockito.when(categoriaRepository.findById(categoriaId)).thenReturn(Optional.of(categoria));
+
+        Categoria result = service.findById(categoriaId, usuarioId);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(categoriaId, result.getId());
+    }
+
+    @Test
+    void findByName() {
+        Long usuarioId = 1L;
+        String nomeBusca = "Saude";
+
+        Categoria cat1 = new Categoria();
+        cat1.setName("Saude");
+        List<Categoria> lista = List.of(cat1);
+
+        Mockito.when(categoriaRepository.findCategoriaByName(usuarioId, nomeBusca)).thenReturn(lista);
+
+        List<Categoria> result = service.findByName(usuarioId, nomeBusca);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals("Saude", result.get(0).getName());
+    }
+
+    @Test
+    void listarCategorias() {
+        Long usuarioId = 1L;
+
+        Categoria cat1 = new Categoria();
+        cat1.setId(10L);
+        List<Categoria> listaEntidades = List.of(cat1);
+
+        CategoriaResponse responseDTO = new CategoriaResponse(10L, usuarioId, "teste",TipoCategoria.RECEITA);
+        List<CategoriaResponse> listaDTO = List.of(responseDTO);
+
+        Mockito.when(categoriaRepository.findAllByUserIdOrPublic(usuarioId)).thenReturn(listaEntidades);
+        Mockito.when(mapper.toResponseList(listaEntidades)).thenReturn(listaDTO);
+
+        List<CategoriaResponse> result = service.listarCategorias(usuarioId);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals(10L, result.get(0).id());
+    }
+}
